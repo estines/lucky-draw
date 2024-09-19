047c2bcffbfceb80e1bd2d14aaab187218620b10
Nopporn Wiriyatharakit <estines.nop@gmail.com>
2024-09-19 20:46:22 +0700

    set up project template

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/lucky-draw', // Replace with your GitHub repository name
+    images: {
+        unoptimized: true,
+    },
+}
+
+export default nextConfig;
